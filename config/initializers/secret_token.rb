74cd7a19dd2bcba2e254f5dba7e24ce44062f897
Michael Grantley <mgrantley@gmail.com>
2013-08-10 15:28:00 -0400

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails3MongoidOmniauth::Application.config.secret_token = 'cf16234abb52a390e1a9c1c18df952f8daa1f9e41558583c33cf507e4838eb81ba6f0d9492371dcb974cc5841b0d3d3ce98de3fd4b8614df8176f6ad33bcf04f'
+Rails3MongoidOmniauth::Application.config.secret_token = 'ecc22886e43c48cfccdc3f1086c975832eadb580d08d51a6fa499af517737d2d2bb71c7a9354fa3a45b12669bb9959a209832147be32aad1d610a33502c59266'
